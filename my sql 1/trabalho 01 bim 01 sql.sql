7b106680378beff5de16d718ea03b9a5b1b523d6
miriamfernandes22c <165180767+miriamfernandes22c@users.noreply.github.com>
2024-03-28 18:17:40 -0300

    Add files via upload

---
+++ my sql 1/trabalho 01 bim 01 sql.sql
@@ -0,0 +1,13 @@
+create database sistema_vendas_ecommerc_22c;
+use sistema_vendas__ecommerc_22c;
+
+create table cliente (
+nome VARCHAR (255) NOT NULL,
+endereco VARCHAR (100) NOT NULL,
+dt_nascimento VARCHAR (100) NOT NULL,
+celular VARCHAR (15),
+email VARCHAR (50),            
+
+
+
+);
